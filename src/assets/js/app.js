2a06bfd98a72246d79b1ae215d717ff22407e8a4
Oren B <Analytics@atomi.local>
2017-07-26 15:51:20 +0300

    added some scripts

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,6 @@
+// Whole-script strict mode syntax
+'use strict';
+
 import $ from 'jquery';
 //import * as segment from 'segment.js';
 
@@ -15,5 +18,40 @@ import Foundation from 'foundation-sites';
 // the line below
 //import './lib/foundation-explicit-pieces';
 
-
 $(document).foundation();
+
+
+
+
+
+//globals
+var pageName,ds,controller, maxRows,pageSize,currentPager,isLoggedIn,loginToken;
+
+//Foundation.Abide.defaults.patterns['baPass'] = /.{6,}/ ;  
+$(document).foundation();
+
+console.log("*** app starting")
+const setPageModule = (pageName) => {     
+console.log(`*** setPageModule( page : ${pageName})`);   
+//ds=new DS(); //data layer
+
+//    var isLogedIn = sessionStorage.getItem('isLogedIn');        
+
+    switch (pageName) {
+        case 'index':    
+            // if(!isLogedIn) window.location="./Login.html"; 
+            //  controller = new LeadTable();
+            //  ds.LeadGet(controller._startDate, controller._endDate, controller._pageSize, controller._currentPage);
+            break;
+        case 'search-form':
+            controller = new SFP();
+            break;
+        case 'search-results':
+            controller = new SRP();
+            break;
+        default:
+            break;
+    }  
+}
+
+
